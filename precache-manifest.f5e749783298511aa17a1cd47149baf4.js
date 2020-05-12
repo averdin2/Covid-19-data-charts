01a538e361f9688b0001257289372de590cd6ffe
averdin2 <alexverdin01@gmail.com>
2020-05-12 16:53:16 -0700

    Updates

---
+++ precache-manifest.f5e749783298511aa17a1cd47149baf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c62c5d774b03a9e3aca0388279769e",
+    "url": "/Covid-19-data-charts/index.html"
+  },
+  {
+    "revision": "8962abc6715f169138f0",
+    "url": "/Covid-19-data-charts/static/css/main.fe89333f.chunk.css"
+  },
+  {
+    "revision": "87af74594e01cc364fe8",
+    "url": "/Covid-19-data-charts/static/js/2.2cc4a11a.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/Covid-19-data-charts/static/js/2.2cc4a11a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8962abc6715f169138f0",
+    "url": "/Covid-19-data-charts/static/js/main.ae8f8023.chunk.js"
+  },
+  {
+    "revision": "c21a466d6283ee27cfb1",
+    "url": "/Covid-19-data-charts/static/js/runtime-main.a081d105.js"
+  }
+]);
